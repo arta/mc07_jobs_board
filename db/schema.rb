a9477e4771c782028919e19a59c4958b9e063cd3
Martin Fencl <martin.fencl@gmail.com>
2017-01-10 22:34:13 -0600

    Asociate Jobs with Users. Migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110173403) do
+ActiveRecord::Schema.define(version: 20170111042151) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 20170110173403) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.integer  "user_id"
     t.index ["category_id"], name: "index_jobs_on_category_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
